3d911253795bbb0fedee52fccdabdea21d638e91
tennylaylor <Lennyttaylor@gmail.com>
2025-03-12 18:28:37 -0400

    udate icon select

--- src/components/gameSession/GameSessionCard.jsx
+++ src/components/gameSession/GameSessionCard.jsx
@@ -1,9 +1,20 @@
 import "./GameCard.scss";
 import placeholderImg from "../../assets/placeholders/placeholder-img.png";
 import UserCircle from "./UserCircle";
-import leaderImg from "../../assets/placeholders/leader.png"; // Add Crown Icon Here
+import leaderImg from "../../assets/placeholders/leader.png"; // Crown Icon Import
+import { useState } from "react";
 
 const GameSessionCard = (props) => {
+  const [selectedPlayers, setSelectedPlayers] = useState(
+    new Array(5).fill(false)
+  );
+
+  const handleSelect = (index, isSelected) => {
+    const updatedSelection = [...selectedPlayers];
+    updatedSelection[index] = isSelected;
+    setSelectedPlayers(updatedSelection);
+  };
+
   const onJoinButtonClick = () => {
     console.log("JOINED!");
   };
@@ -30,13 +41,16 @@ const GameSessionCard = (props) => {
           <h2>{props.game.title}</h2>
           <p>{props.game.description}</p>
 
-          <div className="user-circles">
-            {/* Crown Icon for the Leader */}
+          {/* Crown + 5 Grey Slots Here */}
+          <div className="user-circles" style={{ marginTop: "30px" }}>
             <UserCircle user={{ isLeader: true }} />
 
-            {/* Exactly 5 Grey User Icons for Slots */}
             {[...Array(5)].map((_, index) => (
-              <UserCircle key={index} user={{ isSelected: false }} />
+              <UserCircle
+                key={index}
+                user={{ isSelected: selectedPlayers[index] }}
+                onSelect={(isSelected) => handleSelect(index, isSelected)}
+              />
             ))}
           </div>
         </div>
--- src/components/gameSession/UserCircle.jsx
+++ src/components/gameSession/UserCircle.jsx
@@ -1,23 +1,52 @@
 import "./UserCircle.scss";
 import userImg from "../../assets/placeholders/user-img.png";
-import leaderImg from "../../assets/placeholders/leader.png"; // Crown icon import
+import leaderImg from "../../assets/placeholders/leader.png"; // Crown Icon Import
+import { useState } from "react";
 
-const UserCircle = ({ user }) => {
+const UserCircle = ({ user, onSelect }) => {
+  const [isSelected, setIsSelected] = useState(user.isSelected || false);
+
+  const handleClick = () => {
+    if (!user.isLeader) {
+      const newSelection = !isSelected;
+      setIsSelected(newSelection);
+      if (onSelect) {
+        onSelect(newSelection); // Update parent state
+      }
+    }
+  };
+
   const circleStyle = {
     height: "50px",
     width: "50px",
     borderRadius: "50%",
-    border: user.isSelected ? "3px solid black" : "3px solid #0a78cd",
-    cursor: "pointer",
+    border: isSelected ? "3px solid #333" : "3px solid #0a78cd",
+    cursor: !user.isLeader ? "pointer" : "default", // Leader cannot be selected
   };
 
   return (
-    <div className="user-circle" style={circleStyle}>
-      <img
-        src={user.isLeader ? leaderImg : userImg}
-        alt={user.isLeader ? "Leader" : "User"}
-        style={{ width: "100%", height: "100%" }}
-      />
+    <div
+      className="user-circle"
+      style={circleStyle}
+      onClick={handleClick} // Click logic here
+    >
+      {isSelected ? (
+        <div
+          className="solid-circle"
+          style={{
+            height: "100%",
+            width: "100%",
+            backgroundColor: "#333", // Solid Dark Grey Circle
+            borderRadius: "50%",
+          }}
+        />
+      ) : (
+        <img
+          src={user.isLeader ? leaderImg : userImg}
+          alt={user.isLeader ? "Leader" : "User"}
+          style={{ width: "100%", height: "100%" }}
+        />
+      )}
     </div>
   );
 };
