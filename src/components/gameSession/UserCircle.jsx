36bbf0971c0930ab8dae0c2518dc3eee0e3ae1bc
tennylaylor <Lennyttaylor@gmail.com>
2025-03-20 14:52:02 -0400

    added tooltips functionality for Usercircle

--- src/components/gameSession/UserCircle.jsx
+++ src/components/gameSession/UserCircle.jsx
@@ -1,8 +1,53 @@
 import "./UserCircle.scss";
 import userImg from "../../assets/placeholders/user-img.png";
+import creatorImg from "../../assets/placeholders/isCreator.png"; // Creator Crown
+import { useState } from "react";
 
-const UserCircle = (props) => {
-  return <div>{props.user.pic ? <img src="" /> : <img src={userImg} />}</div>;
+const UserCircle = ({ user, onSelect }) => {
+  const [isSelected, setIsSelected] = useState(user.isSelected || false);
+
+  const handleClick = () => {
+    if (!user.isCreator) {
+      const newSelection = !isSelected;
+      setIsSelected(newSelection);
+      if (onSelect) {
+        onSelect(newSelection); // Update parent state
+      }
+    }
+  };
+
+  const circleStyle = {
+    height: "50px",
+    width: "50px",
+    borderRadius: "50%",
+    backgroundColor: isSelected ? "#333" : "#0a78cd", // Dark Grey on Select
+    cursor: !user.isCreator ? "pointer" : "default",
+    position: "relative", // Required for Tooltip
+  };
+
+  // Tooltip Logic
+  const tooltipText = user.isCreator
+    ? "Creator"
+    : user.isSelected
+    ? `Player ${user.playerNumber}`
+    : "Empty Slot";
+
+  return (
+    <div
+      className="user-circle"
+      style={circleStyle}
+      onClick={handleClick}
+      data-tooltip={tooltipText} // Tooltip Logic
+    >
+      <img
+        src={user.isCreator ? creatorImg : userImg}
+        alt={tooltipText}
+        style={{ width: "100%", height: "100%" }}
+      />
+      {/* Tooltip Display */}
+      <div className="tooltip">{tooltipText}</div>
+    </div>
+  );
 };
 
 export default UserCircle;
